cea61b0fdc644dcc96857b12d0839608ab5c1175
kit <nlipsky@excelsior-usa.com>
2015-11-10 15:48:24 +0600

    Explicit "package" phase execution requirement removed

--- src/main/java/com/excelsior/jet/maven/plugin/JetMojo.java
+++ src/main/java/com/excelsior/jet/maven/plugin/JetMojo.java
@@ -29,10 +29,7 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
 
 import java.io.*;
@@ -46,6 +43,7 @@ import static com.excelsior.jet.Txt.s;
  *
  * @author Nikita Lipsky
  */
+@Execute(phase = LifecyclePhase.PACKAGE)
 @Mojo( name = "build", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class JetMojo extends AbstractMojo {
 
@@ -124,8 +122,9 @@ public class JetMojo extends AbstractMojo {
     private JetHome checkPrerequisites() throws MojoFailureException {
         // first check that main jar were built
         if (!mainJar.exists()) {
-            getLog().error(s("JetMojo.MainJarNotFound.Error", mainJar.getAbsolutePath()));
-            throw new MojoFailureException(s("JetMojo.MainJarNotFound.Failure"));
+            String error = s("JetMojo.MainJarNotFound.Failure", mainJar.getAbsolutePath());
+            getLog().error(error);
+            throw new MojoFailureException(error);
         }
 
         // check main class
