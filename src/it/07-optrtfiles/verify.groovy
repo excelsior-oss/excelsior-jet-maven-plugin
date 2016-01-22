1ed25ef40416200515009c646ddad59fd146a3e8
Thomas Fleischer <thomas.fleischer@iav.de>
2016-01-22 09:20:42 +0100

    Made it test os independent

--- src/it/07-optrtfiles/verify.groovy
+++
@@ -1,7 +0,0 @@
-String ext = System.properties['os.name'].contains("Windows")?".exe":""
-
-File exeFile = new File( basedir, "target/jet/build/HelloWorld" + ext);
-assert exeFile.exists()
-
-File optRtFile = new File( basedir, "target/jet/app/rt/bin/jfxwebkit.dll");
-assert optRtFile.exists()
