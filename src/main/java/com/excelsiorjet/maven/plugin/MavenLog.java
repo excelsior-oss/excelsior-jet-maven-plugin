94dec22e8a14c275f679e44c301705cea27174bd
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-06-16 12:17:29 +0600

    Add comment for MavenLog

--- src/main/java/com/excelsiorjet/maven/plugin/MavenLog.java
+++ src/main/java/com/excelsiorjet/maven/plugin/MavenLog.java
@@ -3,11 +3,14 @@ package com.excelsiorjet.maven.plugin;
 import com.excelsiorjet.api.log.AbstractLog;
 import org.apache.maven.plugin.logging.Log;
 
-public class MavenLog extends AbstractLog {
+/**
+ * Implementation of {@code AbstractLog} that redirects logs into maven system log
+ */
+class MavenLog extends AbstractLog {
 
     private final Log mavenLog;
 
-    public MavenLog(Log mavenLog) {
+    MavenLog(Log mavenLog) {
         this.mavenLog = mavenLog;
     }
 
