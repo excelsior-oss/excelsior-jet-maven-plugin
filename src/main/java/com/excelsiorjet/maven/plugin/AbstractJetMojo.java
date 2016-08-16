b5077eede336d41068d09aad415cb7aded096451
Dmitry Leskov <dleskov@excelsior-usa.com>
2016-08-16 17:39:12 +0600

    Edit README and javadoc for spelling and grammar

--- src/main/java/com/excelsiorjet/maven/plugin/AbstractJetMojo.java
+++ src/main/java/com/excelsiorjet/maven/plugin/AbstractJetMojo.java
@@ -171,9 +171,10 @@ public abstract class AbstractJetMojo extends AbstractMojo {
     protected String execProfilesName;
 
     /**
-     * Command line arguments, that will be passed to the application while startup accelerator profiling run and test run.
+     * Command line arguments that will be passed to the application during startup accelerator profiling
+     * and the test run.
      * You may also set the parameter via the {@code jet.runArgs} system property, where arguments
-     * are comma separated (use "\" to echo comma itself,
+     * are comma separated (use "\" to escape commas within arguments,
      * i.e. {@code -Djet.runArgs="arg1,Hello\, World"} will be passed to your application as {@code arg1 "Hello, World"})
      */
     @Parameter(property = "runArgs")
