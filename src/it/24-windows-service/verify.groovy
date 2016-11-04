78a0f693723a73067d19c033544ec5986b6a65e6
Nikita Lipsky <nlipsky@excelsior-usa.com>
2016-11-04 18:49:15 +0700

    Install/Uninstall windows service scripts creation implemented

--- src/it/24-windows-service/verify.groovy
+++ src/it/24-windows-service/verify.groovy
@@ -2,8 +2,10 @@ import com.excelsiorjet.TestUtils
 
 String ext = TestUtils.exeExt();
 
-File exeFile = new File( basedir, "target/jet/build/SampleService" + ext);
+File exeFile = new File(basedir, "target/jet/build/SampleService" + ext);
 assert exeFile.exists()
 assert exeFile.text.contains("<mainClass>")
-exeFile = new File( basedir, "target/jet/app/SampleService" + ext)
+exeFile = new File(basedir, "target/jet/app/SampleService" + ext)
 assert exeFile.exists()
+assert new File(basedir, "target/jet/app/install.bat").exists()
+assert new File(basedir, "target/jet/app/uninstall.bat").exists()
