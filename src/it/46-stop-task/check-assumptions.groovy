1a262f82e41fbbce4e09f3fffb0371891124b0c9
Nikita Lipsky <nlipsky@excelsior-usa.com>
2018-10-30 19:14:14 +0700

    Fixes in tests

--- src/it/46-stop-task/check-assumptions.groovy
+++ src/it/46-stop-task/check-assumptions.groovy
@@ -1,3 +1,3 @@
 import com.excelsiorjet.TestUtils
 
-return !TestUtils.crossCompilation && System.getProperty("maven.exec")!=null
+return !TestUtils.crossCompilation && System.getProperty("maven.exec")!=null && TestUtils.PGOSupported
